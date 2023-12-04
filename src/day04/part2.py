2116a196925ff285a51e6162516bb51d39d0127e
Stef Robbe <stef.robbe@gmail.com>
2023-12-04 19:16:56 +0100

    part two, day four

--- src/day04/part2.py
+++ src/day04/part2.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import List
 
 
 def read_file(filename) -> str:
@@ -8,8 +9,40 @@ def read_file(filename) -> str:
         return content.strip()
 
 
+def return_clean_nums(nums: str) -> List[int]:
+    return [int(n) for n in nums.split()]
+
+
+def get_matches(a: List[int], b: List[int]) -> set[int]:
+    return set(a) & set(b)
+
+
 def main() -> None:
-    input = read_file("example.txt")
+    input = read_file("input.txt")
+
+    cards = input.splitlines()
+    copies = {}
+    t = 0
+
+    for i in range(len(cards) + 1):
+        copies[i] = 1
+
+    for i, card in enumerate(cards):
+        card = card.split(": ")
+        card.pop(0)
+        card = card[0]
+        splitted = card.split(" | ")
+        winning_nums = return_clean_nums(splitted[0])
+        our_nums = return_clean_nums(splitted[1])
+
+        matching_nums = get_matches(winning_nums, our_nums)
+
+        for j in range(i + 1, i + len(matching_nums) + 1):
+            copies[j] += copies[i]
+
+        t += copies[i]
+
+    print(t)
 
 
 if __name__ == "__main__":
